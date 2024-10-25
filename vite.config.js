326583993f360ea3297da50910f0ecc3a59d57e8
pratik28903 <pratikostwal985@gmail.com>
2024-10-25 11:28:39 +0530

    add file to config url

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
-    base: mode === 'production' ? '/react-performance-app/' : '/', // Use base for production, default for local
+    base: mode === 'production' ? '/react-performance-app/' : '/',
   };
 });
